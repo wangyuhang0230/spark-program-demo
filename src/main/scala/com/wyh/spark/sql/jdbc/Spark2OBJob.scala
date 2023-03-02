b740ab5e0b90bdb3920a16db7a97c9576c293933
WangYuhang <wangyuhang01@thtf.com.cn>
2023-03-02 17:23:04 +0800

    测试 CSV 文件写入 OceanBase 速度

--- src/main/scala/com/wyh/spark/sql/jdbc/Spark2OBJob.scala
+++ src/main/scala/com/wyh/spark/sql/jdbc/Spark2OBJob.scala
@@ -1,52 +1,59 @@
 package com.wyh.spark.sql.jdbc
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import com.wyh.spark.BaseLocalSpark
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.{DataFrame, SaveMode}
 
 /**
  * @author WangYuhang
  * @since 2022-10-27 10:35
  * */
-object Spark2OBJob {
+object Spark2OBJob extends BaseLocalSpark{
   def main(args: Array[String]): Unit = {
-
-    val spark = SparkSession.builder().appName(this.getClass.getSimpleName).master("local[*]").getOrCreate()
-    spark.sparkContext.setLogLevel("error")
 
     import spark.implicits._
 
-    val dssTestDf = spark.read
+//    val dssTestDf = spark.read
+//      .format("jdbc")
+//      .option("url", "jdbc:mysql://10.10.53.25:2883/test")
+////      .option("driver", "org.mariadb.jdbc.Driver")
+//      .option("dbtable", "dss_test")
+//      .option("user", "root@dora#obcluster")
+//      .option("password", "ThtfA25__600100")
+//      .load()
+
+    val obUrl = prop.getProperty("obUrl")
+    val table = prop.getProperty("ob.table")
+    val packet = prop.getProperty("ob.packet")
+    val saveMode = prop.getProperty("ob.save.mode")
+
+    val hisData: DataFrame = spark.read.format("csv")
+      .option("header", "true")
+      .schema(StructType(
+        StructField("UPDATE_TIME", LongType, nullable = false) ::
+          StructField("POINT_ID", StringType, nullable = false) ::
+          StructField("UPLOAD_TIME", LongType, nullable = true) ::
+          StructField("VAL", DoubleType, nullable = true) ::
+          StructField("QUALITY_TYPE", IntegerType, nullable = true) ::
+          StructField("VALID_RANGE", StringType, nullable = true) ::
+          StructField("ZERO_DRIFT", DoubleType, nullable = true) :: Nil)
+      )
+      .load("/user/wyh/hn/data/his_02004_2022")
+      .withColumn("UPDATE_TIME", ($"UPDATE_TIME" / 1000).cast(TimestampType))
+      .withColumn("UPLOAD_TIME", ($"UPLOAD_TIME" / 1000).cast(TimestampType))
+
+    hisData.schema.printTreeString()
+
+    hisData.write
       .format("jdbc")
-      .option("url", "jdbc:mysql://10.10.53.25:2883/test")
-//      .option("driver", "org.mariadb.jdbc.Driver")
-      .option("dbtable", "dss_test")
-      .option("user", "root@dora#obcluster")
-      .option("password", "ThtfA25__600100")
-      .load()
-
-    dssTestDf.show()
-
-
-//    val df = spark.sparkContext.makeRDD(Seq(
-//      (5, "eee"),
-//      (6, "fff"),
-//      (7, "ggg"),
-//      (8, "hhh")
-//    )).toDF("id", "name")
-
-    val df = spark.sparkContext.makeRDD(Seq(
-      "iii", "jjj", "kkk"
-    )).toDF("name")
-
-    df.show()
-
-    df.write
-      .format("jdbc")
-      .mode(SaveMode.Append)
-      .option("url", "jdbc:mysql://10.10.53.25:2883/test")
-      .option("dbtable", "dss_test")
+      .mode(saveMode)
+      .option("url", obUrl)
+      .option("driver", "com.mysql.cj.jdbc.Driver")
+      .option("dbtable", table)
       .option("user", "root@dora#obcluster")
       .option("password", "ThtfA25__600100")
       .option("isolationLevel", "NONE")
+      .option("max_allowed_packet", packet)
       .save()
 
 
