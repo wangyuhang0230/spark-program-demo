8f8bf1ce1d23ffe698e5bad993d14d4683388d3f
WangYuhang <wangyuhang01@thtf.com.cn>
2023-03-13 10:28:37 +0800

    添加 Doris 读取测试代码

--- src/main/scala/com/wyh/spark/hive/SparkReadTestJob.scala
+++ src/main/scala/com/wyh/spark/hive/SparkReadTestJob.scala
@@ -12,7 +12,30 @@ object SparkReadTestJob extends BaseLocalSpark{
 
     import spark.sql
 
-    sql(s"select count(*) hive_count from ${BaseConf.hive_table} where m = ${BaseConf.select_month}").show()
+    val hiveCount = spark.sparkContext.longAccumulator("hive")
+    val dorisCount = spark.sparkContext.longAccumulator("doris")
+    val phoenixCount = spark.sparkContext.longAccumulator("phoenix")
+
+    val hiveDF = sql(s"select * from ${BaseConf.hive_table} where m = ${BaseConf.select_month}")
+    hiveDF.explain()
+    hiveDF.foreach(_ => hiveCount.add(1))
+    println("hiveCount: " + hiveCount.value)
+
+    // Doris 连接器方式
+    val dorisDF = spark.read.format("doris")
+      .option("doris.table.identifier", BaseConf.doris_dbTable)
+      .option("doris.fenodes", BaseConf.doris_fenodes)
+      .option("user", BaseConf.doris_username)
+      .option("password", BaseConf.doris_password)
+      .option("doris.request.tablet.size", BaseConf.doris_tablet_size)
+      .option("doris.batch.size", BaseConf.doris_batch_size)
+      .load()
+      .filter(s"update_time >= '2022-0${BaseConf.select_month}-01 00:00:00'")
+      .filter(s"update_time < '2022-0${BaseConf.select_month + 1}-01 00:00:00'")
+
+    dorisDF.explain()
+    dorisDF.foreach(_ => dorisCount.add(1))
+    println("dorisCount: " + dorisCount.value)
 
     val phoenixDF = spark.read
       .format("org.apache.phoenix.spark")
@@ -26,12 +49,11 @@ object SparkReadTestJob extends BaseLocalSpark{
       .load()
       .filter(s"update_time >= to_timestamp('2022-0${BaseConf.select_month}-01 00:00:00')")
       .filter(s"update_time < to_timestamp('2022-0${BaseConf.select_month + 1}-01 00:00:00')")
-      .selectExpr("count(*) as phoenix_count")
+//      .selectExpr("count(*) as phoenix_count")
 
     phoenixDF.explain()
-//
-//    phoenixDF.show()
-
+    phoenixDF.foreach(_ => phoenixCount.add(1))
+    println("phoenixCount: " + phoenixCount.value)
 
 
 
