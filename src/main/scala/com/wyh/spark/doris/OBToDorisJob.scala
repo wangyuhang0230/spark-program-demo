c1ecf7a19ddc9b42e90044455ed691f92f0ccb20
WangYuhang <wangyuhang01@thtf.com.cn>
2023-05-31 18:27:12 +0800

    测试 jdbc 批量拉取

--- src/main/scala/com/wyh/spark/doris/OBToDorisJob.scala
+++ src/main/scala/com/wyh/spark/doris/OBToDorisJob.scala
@@ -30,14 +30,20 @@ object OBToDorisJob extends BaseLocalSpark{
 //      .option("isolationLevel", "NONE")
 //      .load()
 
-    val array = (1 to 365)
+    val interval = 1
+    val array = (1 to (366*4/interval) + 1)
       .map(i => {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-        val startDate = LocalDate.ofYearDay(2021, i).atTime(0, 0, 0)
-        val endDate = startDate.plusDays(1)
+        val time = LocalDate.ofYearDay(2022, 1).atTime(0, 0, 0)
+//        val startDate = time.plusDays((i - 1) * interval)
+//        val endDate = startDate.plusDays(interval)
+        val startDate = time.plusHours((i - 1) * interval * 6)
+        val endDate = startDate.plusHours(interval * 6)
         s"update_time >= '${formatter.format(startDate)}' and update_time < '${formatter.format(endDate)}'"
       })
       .toArray
+
+    println(BaseConf.ob_table + " ===> " + BaseConf.doris_dbTable)
 
     println("array:")
     println(array.mkString("\n"))
@@ -45,8 +51,14 @@ object OBToDorisJob extends BaseLocalSpark{
     val properties = new Properties();
     properties.put("user", BaseConf.ob_username)
     properties.put("password", BaseConf.ob_password)
+    properties.put("driver", "com.mysql.cj.jdbc.Driver")
+    properties.put("max_allowed_packet", BaseConf.ob_packet)
+    properties.put("fetchsize", "50000")
+    properties.put("readBatchSize", "50000")
+    properties.put("useCursorFetch", "true")
 
     val obDF = spark.read
+      .option("fetchsize", "50000")
       .jdbc(BaseConf.ob_url, BaseConf.ob_table, array, properties)
       .withColumn("quality_type", lit(null))
       .withColumn("valid_range", lit(null))
@@ -58,10 +70,10 @@ object OBToDorisJob extends BaseLocalSpark{
     // Doris 连接器方式
     obDF
       .write.format("doris")
-      .option("driver", "com.mysql.cj.jdbc.Driver")
+//      .option("driver", "com.mysql.cj.jdbc.Driver")
       .option("doris.table.identifier", BaseConf.doris_dbTable)
       .option("doris.fenodes", BaseConf.doris_fenodes)
-      .option("sink.batch.size", "50000")
+      .option("sink.batch.size", "500000")
       .option("sink.max-retries", "1")
       .option("user", BaseConf.doris_username)
       .option("password", BaseConf.doris_password)
